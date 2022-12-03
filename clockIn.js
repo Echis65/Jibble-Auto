7328d18fcde1c2620b5a614694154bd94b690d91
Echis65 <Evansatompoya@gmail.com>
2022-12-03 07:20:14 +0000

    Testing of scripts

---
+++ clockIn.js
@@ -0,0 +1,37 @@
+import { Builder, By, until } from "selenium-webdriver";
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const driver = new Builder().forBrowser('chrome').build()
+const clockInScript = async () => {
+    await driver.get('https://jibble.io/app')
+
+    //getting email and password from .env file
+    let email = process.env.EMAIL
+    let password = process.env.PASSWORD
+    let emailInput = await driver.findElement(By.xpath('/html/body/div[1]/div/div[1]/div/div/div[2]/div/div/div/div/div[2]/form/div[1]/div/div/div/input'))
+    const passwordInput  = await driver.findElement(By.xpath('/html/body/div[1]/div/div[1]/div/div/div[2]/div/div/div/div/div[2]/form/div[2]/div/div/div/div[1]/input'))
+    let loginButton = await driver.findElement(By.xpath('/html/body/div[1]/div/div[1]/div/div/div[2]/div/div/div/div/div[2]/button'))
+    await emailInput.sendKeys(email)
+    await passwordInput.sendKeys(password)
+    await loginButton.click()
+
+    //Implicitly waiting till clock in button is found and clicked
+    await driver.manage().setTimeouts( { implicit: 20000 } );
+    let clockInButton = await driver.findElement(By.xpath('/html/body/div[1]/div[1]/div[1]/header/div/div/div[2]/div/div[2]/button'))
+    await clockInButton.click()
+
+    //Implicitly waiting till save button is found and clicked
+    await driver.manage().setTimeouts( { implicit: 20000 } );
+    let saveButton = await driver.findElement(By.xpath('/html/body/div[1]/div[2]/div[1]/aside/div[1]/div/div[3]/button[2]'))   
+    await saveButton.click();
+    await driver.manage().setTimeouts( { implicit: 20000 } );
+    let clockOutButton = await driver.findElement(By.xpath('/html/body/div[1]/div/div[1]/header/div/div/div[2]/div/div/button[3]'))  
+
+    //explicitly waiting until clockOut button is visible before quitting the browser
+    await driver.wait(until.elementIsVisible(clockOutButton))
+    driver.quit()
+}
+
+clockInScript()
---
+++ clockOut.js
@@ -0,0 +1,38 @@
+import { Builder, By, until } from "selenium-webdriver";
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const driver = new Builder().forBrowser('chrome').build();
+
+const clockOutScript = async() => {
+    await driver.get('https://jibble.io/app')
+
+    //getting email and password from .env file
+    let email = process.env.EMAIL
+    let password = process.env.PASSWORD
+    let emailInput = await driver.findElement(By.xpath('/html/body/div[1]/div/div[1]/div/div/div[2]/div/div/div/div/div[2]/form/div[1]/div/div/div/input'))
+    const passwordInput  = await driver.findElement(By.xpath('/html/body/div[1]/div/div[1]/div/div/div[2]/div/div/div/div/div[2]/form/div[2]/div/div/div/div[1]/input'))
+    await emailInput.sendKeys(email)
+    await passwordInput.sendKeys(password)
+    let loginButton = await driver.findElement(By.xpath('/html/body/div[1]/div/div[1]/div/div/div[2]/div/div/div/div/div[2]/button'))
+    await loginButton.click()
+    
+    //Waiting for the clockOut button to be found and clicked
+    await driver.manage().setTimeouts( { implicit: 20000 } );
+    let clockOutButton = await driver.findElement(By.xpath('/html/body/div[1]/div/div[1]/header/div/div/div[2]/div/div/button[3]'))
+    await clockOutButton.click()
+
+    //Waiting for the save button to be found and clicked
+    await driver.manage().setTimeouts({ implicit: 10000});
+    let saveButton = await driver.findElement(By.xpath('/html/body/div[1]/div[2]/div[1]/aside/div[1]/div/div[3]/button[2]'))
+    await saveButton.click()
+    await driver.manage().setTimeouts( { implicit: 20000 } );
+    let clockInButton = await driver.findElement(By.xpath('/html/body/div[1]/div[2]/div[1]/header/div/div/div[2]/div/div[2]/button'))
+
+    //explicitly waiting until clockIn button is visible before quitting the browser
+    await driver.wait(until.elementIsVisible(clockInButton))
+    driver.quit()
+}
+
+clockOutScript();
